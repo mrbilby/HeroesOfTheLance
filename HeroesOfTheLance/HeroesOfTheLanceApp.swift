fff767bb558aaa6de334465445336346d8f29350
mrbilby <baileymrb@gmail.com>
2023-03-29 15:03:25 +0100

    Initial Commit

---
+++ HeroesOfTheLance/HeroesOfTheLanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesOfTheLanceApp.swift
+//  HeroesOfTheLance
+//
+//  Created by James Bailey on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroesOfTheLanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
